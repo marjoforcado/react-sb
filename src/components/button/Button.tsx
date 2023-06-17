e60ce5e10d25705d1074a4aa08d98086941e11dc
Marjo Forcado <marjoforcado@gmail.com>
2023-06-17 14:37:09 +0800

    Refactor code

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './button.css';
+import React from "react";
+import styles from "./styles.module.scss";
 
 interface ButtonProps {
   /**
@@ -13,7 +13,7 @@ interface ButtonProps {
   /**
    * How large should the button be?
    */
-  size?: 'small' | 'medium' | 'large';
+  size?: "small" | "medium" | "large";
   /**
    * Button contents
    */
@@ -29,16 +29,22 @@ interface ButtonProps {
  */
 export const Button = ({
   primary = false,
-  size = 'medium',
+  size = "medium",
   backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? styles["storybook-button--primary"]
+    : styles["storybook-button--secondary"];
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={[
+        styles["storybook-button"],
+        styles[`storybook-button--${size}`],
+        mode,
+      ].join(" ")}
       style={{ backgroundColor }}
       {...props}
     >
