e671784f3913af9bce01f24e9483062e390aafda
Marjo Forcado <marjoforcado@gmail.com>
2023-06-17 15:22:33 +0800

    Add config

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+
+// This is required to read package.json file when
+// using Native ES modules in Node.js
+// https://rollupjs.org/command-line-interface/#importing-package-json
+import { createRequire } from "node:module";
+const requireFile = createRequire(import.meta.url);
+const packageJson = requireFile("./package.json");
+
+const config = [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript(),
+      postcss({
+        modules: true,
+        extensions: [".css"],
+      }),
+    ],
+  },
+  {
+    input: "dist/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts.default()],
+    external: [/\.css$/],
+  },
+];
+
+export default config;
