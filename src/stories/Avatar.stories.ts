59ed6c4bc870e16cb22950882cc4d87ede751a43
Marjo Forcado <marjoforcado@gmail.com>
2023-06-17 16:00:00 +0800

    Create Avatar.stories.ts

---
+++ src/stories/Avatar.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Avatar } from "../components";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: "Example/Avatar",
+  component: Avatar,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Avatar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {};
